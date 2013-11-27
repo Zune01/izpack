01e93c8fbd2b976808b4974de64999d016f11128
Andreas Magnusson <andreas.ch.magnusson@gmail.com>
2013-11-27 13:38:30 +0100

    Fix the test as well...

--- izpack-tools/src/test/java/com/izforge/izpack/util/PlatformsTest.java
+++ izpack-tools/src/test/java/com/izforge/izpack/util/PlatformsTest.java
@@ -24,6 +24,7 @@ import static org.junit.Assert.assertEquals;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
@@ -193,9 +194,9 @@ public class PlatformsTest extends AbstractPlatformTest
         {
 
             @Override
-            protected String getReleasePath()
+            protected List<String> getReleasePath()
             {
-                return "";
+                return Collections.emptyList();
             }
 
             @Override
